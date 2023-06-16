08e6d8ec192268d948a737988450891ae2c143ad
DevendraBandaru <bandarudevendra63057@gmail.com>
2023-06-16 12:24:46 +0530

    added

--- SpringWorkShop/src/main/java/org/jsp/SpringWorkShop/controller/UserController.java
+++ SpringWorkShop/src/main/java/org/jsp/SpringWorkShop/controller/UserController.java
@@ -9,6 +9,7 @@ import org.jsp.SpringWorkShop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class UserController {
 	@Autowired
 	private UserService service;
